4ca71e09d6adabb6f5324fcafe367593d6d1dc4e
ipoca85 <hong.chen@ipoca.jp>
2018-05-07 12:07:31 +0900

    catch error.

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -15,15 +15,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create line_item" do
-    assert_difference('LineItem.count') do
-      post line_items_url, params: { product_id: products(:ruby).id }
-    end
-
-    follow_redirect!
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
-  end
+  # test "should create line_item" do
+  #   assert_difference('LineItem.count') do
+  #     post line_items_url, params: { product_id: products(:ruby).id }
+  #   end
+  #
+  #   follow_redirect!
+  #   assert_select 'h2', 'Your Pragmatic Cart'
+  #   assert_select 'li', 'Programming Ruby 1.9'
+  # end
 
   test "should show line_item" do
     get line_item_url(@line_item)
@@ -47,4 +47,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
+
+  test "should create line_item" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id:products(:ruby).id }
+    end
+    follow_redirect!
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
+  end
+
 end
