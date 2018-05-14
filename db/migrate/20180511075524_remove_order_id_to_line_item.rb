cc34e2e75b1103f723639d071f8fdfde45b1fa1b
ipoca85 <hong.chen@ipoca.jp>
2018-05-14 17:49:04 +0900

    start react.

---
+++ db/migrate/20180511075524_remove_order_id_to_line_item.rb
@@ -0,0 +1,5 @@
+class RemoveOrderIdToLineItem < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :line_items, :order_id, :integer
+  end
+end
---
+++ db/migrate/20180511075914_add_order_to_line_item3.rb
@@ -0,0 +1,5 @@
+class AddOrderToLineItem3 < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :line_items, :order, foreign_key: true
+  end
+end
